04208bfb131b1f59bcbcffe9458e5693421b0bff
azizgm1234 <140641043+azizgm1234@users.noreply.github.com>
2023-12-03 22:31:27 +0100

    updated2

--- src/main/java/tn/esprit/se/projet1/loggingaspect/LoggingAspect.java
+++ src/main/java/tn/esprit/se/projet1/loggingaspect/LoggingAspect.java
@@ -2,9 +2,7 @@ package tn.esprit.se.projet1.loggingaspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,4 +16,21 @@ import org.springframework.stereotype.Component;
             String name = joinPoint.getSignature().getName();
             log.info("In method " + name + " : ");
         }
+
+    @AfterReturning("execution(* tn.esprit.se.projet1.services.EtudiantService.addEtudiant(..))")
+    public void logMethodEntryReturning(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In running " + name + " : ");
+    }
+
+    @AfterThrowing("execution(* tn.esprit.se.projet1.services.EtudiantService.addEtudiant(..))")
+    public void logMethodEntryThrowing(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In throwing " + name + " : ");
+    }
+    @After("execution(* tn.esprit.se.projet1.services.EtudiantService.addEtudiant(..))")
+    public void logMethodEntryExit(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("after Execution " + name + " : ");
+    }
     }
